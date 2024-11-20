8b565259d728af8856c81bbcd6ef39042087546e
highreso_shika <highreso_shika@MOBILE-HH.localdomain>
2024-11-20 16:18:12 +0900

    ステップ2

--- 9cc.c
+++ 9cc.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+    char buf[256];
     int number;
-    scanf("%d", &number);
+    scanf("%255s", buf);
+
+    number = buf[0] - '0';
     printf("%d\n", number);
 }
